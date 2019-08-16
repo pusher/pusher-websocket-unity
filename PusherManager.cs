f097e6ec94d2cf24388c11bcab145c45d788fef1
Damiano Donati <damiano.donati@gmail.com>
2019-08-16 15:00:38 +0100

    reorg for upm

---
+++ PusherManager.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Threading.Tasks;
+using PusherClient;
+using UnityEngine;
+
+public class PusherManager : MonoBehaviour
+{
+    // A mutation of https://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial/writing-game-manager
+    public static PusherManager instance = null;
+    private Pusher _pusher;
+    private Channel _channel;
+    private const string APP_KEY = "APP_KEY";
+    private const string APP_CLUSTER = "APP_CLUSTER";
+
+    async Task Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        await InitialisePusher();
+        Console.WriteLine("Starting");
+    }
+
+    private async Task InitialisePusher()
+    {
+        //Environment.SetEnvironmentVariable("PREFER_DNS_IN_ADVANCE", "true");
+
+        if (_pusher == null && (APP_KEY != "APP_KEY") && (APP_CLUSTER != "APP_CLUSTER"))
+        {
+            _pusher = new Pusher(APP_KEY, new PusherOptions()
+            {
+                Cluster = APP_CLUSTER
+            });
+
+            _pusher.Error += OnPusherOnError;
+            _pusher.ConnectionStateChanged += PusherOnConnectionStateChanged;
+            _pusher.Connected += PusherOnConnected;
+            _channel = await _pusher.SubscribeAsync("my-channel");
+            _channel.Subscribed += OnChannelOnSubscribed;
+            await _pusher.ConnectAsync();
+        }
+        else
+        {
+            Debug.LogError("APP_KEY and APP_CLUSTER must be correctly set. Find how to set it at https://dashboard.pusher.com");
+        }
+    }
+
+    private void PusherOnConnected(object sender)
+    {
+        Debug.Log("Connected");
+        _channel.Bind("my-event", (dynamic data) =>
+        {
+            Debug.Log("my-event received");
+        });
+    }
+
+    private void PusherOnConnectionStateChanged(object sender, ConnectionState state)
+    {
+        Debug.Log("Connection state changed");
+    }
+
+    private void OnPusherOnError(object s, PusherException e)
+    {
+        Debug.Log("Errored");
+    }
+
+    private void OnChannelOnSubscribed(object s)
+    {
+        Debug.Log("Subscribed");
+    }
+
+    public void Message(string message)
+    {
+        _channel?.Trigger("time has occured", message);
+    }
+
+    async Task OnApplicationQuit()
+    {
+        if (_pusher != null)
+        {
+            await _pusher.DisconnectAsync();
+        }
+    }
+}
